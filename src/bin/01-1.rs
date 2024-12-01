2a9ccd9e64aae1cf195176543c04d1504958a9ad
Vijay Ramamurthy <vrama628@gmail.com>
2024-12-01 08:57:01 -0600

    01-1

--- src/bin/01-1.rs
+++ src/bin/01-1.rs
@@ -1,3 +1,23 @@
+use itertools::Itertools;
+use std::io::stdin;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut list1, mut list2): (Vec<usize>, Vec<usize>) = stdin()
+        .lines()
+        .map(|line| {
+            line.unwrap()
+                .split_whitespace()
+                .map(|n| n.parse::<usize>().unwrap())
+                .collect_tuple()
+                .unwrap()
+        })
+        .unzip();
+    list1.sort();
+    list2.sort();
+    let result: usize = list1
+        .into_iter()
+        .zip(list2)
+        .map(|(a, b)| a.abs_diff(b))
+        .sum();
+    println!("{result}");
 }
